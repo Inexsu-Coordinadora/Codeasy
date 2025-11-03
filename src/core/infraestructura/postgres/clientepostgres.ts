25b54509fe3a9c542fde9a0b5b6783dbf534a46c
DinaMartinezpantoja15 <dinamartinezpant@gmail.com>
2025-11-03 15:51:24 -0500

    se agregan cambios de carpeta bruno y configuracion

--- src/core/infraestructura/postgres/clientepostgres.ts
+++ src/core/infraestructura/postgres/clientepostgres.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
-import { configuration } from "../../../common/configuraciones.js";
+import { configuration } from "../../../common/configuracion.js";
 
 const pool = new Pool({
   host: configuration.baseDatos.host,
