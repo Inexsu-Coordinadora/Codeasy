bc191e0093786487917b9ccb090f73d074d0ac3f
Andres Elias <andreselias58690@gmail.com>
2025-11-03 15:51:17 -0500

    cambios configuracion

--- src/core/infraestructura/postgres/clientePostgres.ts
+++ src/core/infraestructura/postgres/clientePostgres.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
-import { configuration } from "../../../common/config";
+import { configuration } from "../../../common/configuracion";
 
 const pool = new Pool({
   host: configuration.baseDatos.host,
